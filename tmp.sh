7969b6107821a99b0c593d7db2df4dceb9e2c567
Alexander <alexander.v000@gmail.com>
2018-04-02 09:16:38 +0300

    hw16: run env template

--- tmp.sh
+++ tmp.sh
@@ -11,3 +11,14 @@ gcloud compute firewall-rules create reddit-app \
 --target-tags=docker-machine \
 --description="Allow TCP connections" \
 --direction=INGRESS
+
+
+docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
+docker run -d --network=reddit --network-alias=comment alxbird/comment:1.0
+docker run -d --network=reddit --network-alias=post alxbird/post:1.0
+docker run -d --network=reddit -p 9292:9292 alxbird/ui:1.0
+
+COMMENT_SERVICE_HOST
+POST_SERVICE_HOST
+COMMENT_DATABASE_HOST
+POST_DATABASE_HOST
