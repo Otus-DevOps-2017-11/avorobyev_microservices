4b4541386c100a6f7dfa12a32e95f9d86b07de5e
Alexander <alexander.v000@gmail.conf>
2018-04-02 20:14:24 +0300

    hw16: tried with changed aliases

--- tmp.sh
+++ tmp.sh
@@ -12,13 +12,26 @@ gcloud compute firewall-rules create reddit-app \
 --description="Allow TCP connections" \
 --direction=INGRESS
 
+docker pull mongo:latest
+docker build -t alxbird/post:1.0 ./post-py
+docker build -t alxbird/comment:1.0 ./comment
+docker build -t alxbird/ui:1.0 ./ui
+
+docker network create reddit
 
 docker run -d --network=reddit --network-alias=post_db --network-alias=comment_db mongo:latest
 docker run -d --network=reddit --network-alias=comment alxbird/comment:1.0
 docker run -d --network=reddit --network-alias=post alxbird/post:1.0
 docker run -d --network=reddit -p 9292:9292 alxbird/ui:1.0
 
-COMMENT_SERVICE_HOST
-POST_SERVICE_HOST
-COMMENT_DATABASE_HOST
-POST_DATABASE_HOST
+cat > envfile <<-!
+COMMENT_SERVICE_HOST=commenter
+POST_SERVICE_HOST=poster
+COMMENT_DATABASE_HOST=commenter_db
+POST_DATABASE_HOST=poster_db
+!
+
+docker run -d --network=reddit --network-alias=poster_db --network-alias=commenter_db mongo:latest
+docker run -d --network=reddit --network-alias=commenter --env-file=./envfile alxbird/comment:1.0
+docker run -d --network=reddit --network-alias=poster --env-file=./envfile alxbird/post:1.0
+docker run -d --network=reddit -p 9292:9292 --env-file=./envfile alxbird/ui:1.0
